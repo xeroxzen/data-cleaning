ce797425dfe2a27563b9c5591179b6457b576024
Google Jr <andilembele020@gmail.com>
2023-07-03 20:10:57 +0200

    fix: another iteration

--- smart_parser_original.py
+++ smart_parser_original.py
@@ -23,38 +23,23 @@ def excel_to_csv(excel_file):
 
 
 def clean_csv(csv_file):
-    # Load the CSV file into a Pandas dataframe
     try:
         df = pd.read_csv(csv_file)
     except FileNotFoundError:
         print(f"Error: {csv_file} not found")
         return
 
-    # Remove empty columns
     df = df.dropna(axis=1, how='all')
 
-    # Remove duplicates based on the 'Brand' column and keep the row with the longest description
-    df = df.sort_values('Text', key=lambda col: col.str.len(), ascending=False).drop_duplicates('Brand').reset_index(drop=True)
+    pattern = r'(?i)\b((\d+°?\s*EAST)?\s*Hyōgo\s*(?:Dry\s*)?(?:Gin|Black Edition Gin)?\s*(?:\d+x\s*\d+\s*Tonic Water)?)\b|\b((\d+)\s*James\s*E\.\s*Pepper\s*Straight\s*(?:Whiskey|Rye|Bourbon)\s*\d*\s*(Proof)?)\b'
 
-    # Convert 'Alcohol_Content' column values to percentages
-    # Unused, ideally this was to be used to convert values like '12,5%' to '12.5%'
-    def convert_percent(x):
-        if isinstance(x, str) and x.endswith('%'):
-            return float(x[:-1]) * 100
-        else:
-            return x
+    df['Pattern'] = df['Brand'].apply(lambda x: re.findall(
+        pattern, x)[0] if re.findall(pattern, x) else x)
 
-    # df['Alcohol_Content'] = df['Alcohol_Content'].apply(lambda x: str(int(float(x.replace(',', '.'))*100))+'%' if 
-    # ',' in str(x) else str(x))
-    df['Alcohol_Content'] = df['Alcohol_Content'].apply(lambda x: re.findall(r'\d+\.\d+', str(x))[0] if re.findall(r'\d+\.\d+', str(x)) else str(x))
-    df['Alcohol_Content'] = df['Alcohol_Content'].apply(lambda x: str(int(float(x)*100))+'%' if '.' in str(x) else str(x))
+    df = df.drop_duplicates(subset='Pattern')
 
-    # Fill missing fields with data from duplicates as long as the 'Brand' column is the same, as was specified in 
-    # the task
-    df = df.groupby('Brand').apply(lambda x: x.ffill().bfill())
+    df = df.drop(columns=['Pattern'])
 
-    # Save the cleaned dataframe to a new CSV file
-    assert isinstance(csv_file, object)
     cleaned_file = os.path.splitext(csv_file)[0] + '_cleaned.csv'
     df.to_csv(cleaned_file, index=False)
     print("Cleaned CSV file")
@@ -62,21 +47,20 @@ def clean_csv(csv_file):
 
 def csv_to_excel(csv_file):
     try:
-        # Load the CSV file into a Pandas dataframe
         df = pd.read_csv(csv_file)
 
-        # Highlight rows with fuzzy matches in the 'Brand' column
         brand_set = set(df['Brand'].tolist())
         fuzzy_matches = []
         for brand in brand_set:
-            matches = process.extract(brand, brand_set, scorer=fuzz.partial_ratio, limit=None)
+            matches = process.extract(
+                brand, brand_set, scorer=fuzz.partial_ratio, limit=None)
             for match in matches:
                 if match[1] >= 90 and match[0] != brand:
                     fuzzy_matches.append(match[0])
         df['is_fuzzy_match'] = df['Brand'].isin(fuzzy_matches)
-        df = df.style.apply(lambda x: ['background-color: yellow' if x['is_fuzzy_match'] else '' for i in x], axis=1).hide_columns(['is_fuzzy_match'])
+        df = df.style.apply(lambda x: ['background-color: yellow' if x['is_fuzzy_match']
+                            else '' for i in x], axis=1).hide_columns(['is_fuzzy_match'])
 
-        # Save the dataframe to a new Excel file
         excel_file = os.path.splitext(csv_file)[0] + '_converted_back_to.xlsx'
         df.to_excel(excel_file, index=False)
         print("Converted CSV file to Excel file")
@@ -84,13 +68,38 @@ def csv_to_excel(csv_file):
         print(f"Error occurred while converting CSV file to Excel: {str(err)}")
 
 
+def remove_duplicates(excel_file):
+    try:
+        df = pd.read_excel(excel_file)
+
+        pattern = r'(?i)\b((\d+°?\s*EAST)?\s*Hyōgo\s*(?:Dry\s*)?(?:Gin|Black Edition Gin)?\s*(?:\d+x\s*\d+\s*Tonic Water)?)\b|\b((\d+)\s*James\s*E\.\s*Pepper\s*Straight\s*(?:Whiskey|Rye|Bourbon)\s*\d*\s*(Proof)?)\b'
+
+        df['Pattern'] = df['Brand'].apply(lambda x: re.findall(
+            pattern, x)[0] if re.findall(pattern, x) else x)
+
+        df = df.drop_duplicates(subset='Pattern')
+
+        df = df.drop(columns=['Pattern'])
+
+        cleaned_file = os.path.splitext(excel_file)[0] + '_cleaned.xlsx'
+        df.to_excel(cleaned_file, index=False)
+        print("Removed duplicates from Excel file")
+    except Exception as err:
+        print(
+            f"Error occurred while removing duplicates from Excel file: {str(err)}")
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Excel/CSV cleaning tool')
 
-    parser.add_argument('--xl', metavar='EXCEL_FILE', type=str, help='convert Excel file to CSV')
-    parser.add_argument('--clean', metavar='CSV_FILE', type=str, help='clean a CSV file or directory')
-    parser.add_argument('--csv', metavar='CSV_FILE', type=str, help='convert CSV file to Excel')
+    parser.add_argument('--xl', metavar='EXCEL_FILE',
+                        type=str, help='convert Excel file to CSV')
+    parser.add_argument('--clean', metavar='CSV_FILE',
+                        type=str, help='clean a CSV file or directory')
+    parser.add_argument('--csv', metavar='CSV_FILE',
+                        type=str, help='convert CSV file to Excel')
+    parser.add_argument('--rd', metavar='EXCEL_FILE',
+                        type=str, help='remove duplicates from Excel file')
 
     args = parser.parse_args()
 
@@ -99,15 +108,15 @@ if __name__ == '__main__':
             excel_to_csv(args.xl)
         elif args.clean:
             if os.path.isdir(args.clean):
-                # Clean all CSV files in a directory
                 for file in os.listdir(args.clean):
                     if file.endswith('.csv'):
                         clean_csv(os.path.join(args.clean, file))
             else:
-                # Clean a single CSV file
                 clean_csv(args.clean)
         elif args.csv:
             csv_to_excel(args.csv)
+        elif args.rd:
+            remove_duplicates(args.rd)
         else:
             parser.print_help()
     except Exception as e:
