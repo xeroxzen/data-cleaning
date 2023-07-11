a44080fa3cfd48e065f93b685a6494c5dbe41e6c
Google Jr <andilembele020@gmail.com>
2023-07-11 17:54:02 +0200

    feat: Bash helper files

---
+++ clean_excel_files.cmd
@@ -0,0 +1,4 @@
+@echo off
+cls
+python "/Users/andilejaden/Workspace/iQuaint/data_engineering/data_cleaning/python/excel_parser.py --clean" %1
+@pause
---
+++ remove_duplicates.cmd
@@ -0,0 +1,4 @@
+@echo off
+cls
+python "/Users/andilejaden/Workspace/iQuaint/data_engineering/data_cleaning/python/excel_parser.py --rd" %1
+@pause
